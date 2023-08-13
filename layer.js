cec92b0a6408977133d2c74069f3308b3dbf0377
4mishra <dhiraj.raodev@gmail.com>
2023-08-14 01:45:36 +0530

    layer support functionality

---
+++ layer.js
@@ -0,0 +1,24 @@
+// Function to add a new layer
+function addLayer() {
+    layers.push(document.createElement("canvas"));
+    layers[layers.length - 1].width = canvas.width;
+    layers[layers.length - 1].height = canvas.height;
+    layerSelect.appendChild(new Option(`Layer ${layers.length}`, layers.length - 1));
+    setActiveLayer(layers.length - 1);
+}
+
+// Function to remove the active layer
+function removeLayer() {
+    if (layers.length > 1) {
+        layers.splice(activeLayerIndex, 1);
+        layerSelect.remove(activeLayerIndex);
+        setActiveLayer(Math.max(activeLayerIndex - 1, 0));
+    }
+}
+
+// Function to set the active layer
+function setActiveLayer(index) {
+    activeLayerIndex = index;
+    layerSelect.selectedIndex = index;
+    redrawCanvas();
+}
